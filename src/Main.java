310244ae02548dc771c4cec06708c64dd1a9abee
noelglaess <uqhkm@atudent.kit.edu>
2026-02-17 16:22:36 +0100

    1

---
+++ src/Main.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hallo! Das Projekt ist bereit.");
+        System.out.println("Java Version: " + System.getProperty("java.version"));
+    }
+}
