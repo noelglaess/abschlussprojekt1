c21d4a5aafc01075ddf7e04544bb7049799aa610
noelglaess <noel.glaess@gmail.com>
2026-02-21 17:49:27 +0100

    1

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hallo! Das Projekt ist bereit.");
-        System.out.println("Java Version: " + System.getProperty("java.version"));
+
     }
 }
